004c97ba9bff9e5a4b90b47c93f4ac3c511143ea
chamel chowk <chamelchowk@outlook.com>
2025-06-29 22:50:00 +0300

    generating a random students database and Querying for Java students.

--- src/com/shamel/Main.java
+++ src/com/shamel/Main.java
@@ -1,11 +1,28 @@
 package com.shamel;
 
+import com.shamel.model.Student;
+import com.shamel.util.QueryList;
+
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
 
+        //Generating a list of 25 random students:
+        List<Student> randomStudents = Student.generateRandomStudentDataBase(25);
+        randomStudents.forEach(System.out::println);
 
+        System.out.print("\n".repeat(3));
 
+        QueryList<Student> studentsTobeSearhced = new QueryList<>();
+        studentsTobeSearhced.addAll(randomStudents);
+
+        // Obtaining the list of student enrolled in Java
+        List<Student> JavaStudents =studentsTobeSearhced.getMatches("course","Java");
+
+        // we will print the list of Java Students here:
+        JavaStudents.forEach(System.out::println);
 
     }
 
--- src/com/shamel/model/Student.java
+++ src/com/shamel/model/Student.java
@@ -8,22 +8,22 @@ import java.util.Random;
 
 public class Student implements Comparable<Student>, QueryItem {
 
-    protected final String name;
-    protected final int yearStarted;
-    protected final String course;
+    final String name;
+    final int yearStarted;
+    final String course;
 
     private static int IDmaker = 1000;
     private final int studentID;
 
-    protected static Random random =new Random();
+     static Random random =new Random();
     private static String[] firstNames = {"Andres", "Omar", "Mathew", "Dan", "Abby", "Han"};
-    private static String[] courses = {"Java", "Calculus", "C++"};
+    private static String[] courses = {"Java", "Calculus", "C++", "C#", "RF design"};
 
 
     public Student() {
 
         this.name = firstNames[random.nextInt(firstNames.length -1)];
-        this.course = courses[random.nextInt(firstNames.length -1)];
+        this.course = courses[random.nextInt(courses.length -1)];
         this.yearStarted = random.nextInt(2019, 2023);
         this.studentID = IDmaker++;
 
@@ -47,8 +47,10 @@ public class Student implements Comparable<Student>, QueryItem {
         };
     }
 
-
-
+    @Override
+    public String toString() {
+        return "{%-10s %-10d %-10s".formatted(name, yearStarted, course+"}");
+    }
 
     // this is a static method to generate a Randomized Student databased written into a
     // List, we will use this method to test our methods and classes in this package:
